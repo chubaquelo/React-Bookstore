70e90766bea8f15b2d72cef2080b42aac0d08a25
EstebanP <epinczinger@gmail.com>
2021-04-07 19:05:06 -0300

    add changeFilter action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
-export const addBook = book => (
-  {
-    type: 'CREATE_BOOK',
-    payload: book,
-  }
-);
+export const addBook = book => ({
+  type: 'CREATE_BOOK',
+  payload: book,
+});
 
 export const removeBook = bookId => ({
   type: 'REMOVE_BOOK',
   payload: bookId,
+});
+
+export const changeFilter = category => ({
+  type: 'CHANGE_FILTER',
+  payload: category,
 });
