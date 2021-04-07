9e3f7a6936f9d633531224a3b0a6b3390d3cc467
EstebanP <epinczinger@gmail.com>
2021-04-07 13:43:07 -0300

    add book reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,14 @@
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
 const bookReducer = (state = [], action) => {
   switch (action.type) {
+      case CREATE_BOOK: {
+          return [...state, action.payload]
+      }
+      case REMOVE_BOOK: {
+            return [...state].filter(book=>book.id !== action.payload)
+      }
     default:
       return state;
   }
