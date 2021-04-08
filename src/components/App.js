19963362097e4debf16d24f9a40247f14458b398
EstebanP <epinczinger@gmail.com>
2021-04-08 17:08:26 -0300

    style buttons on bookcard

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
 
 const App = () => (
-  <div className="bg-gray-100">
+  <div className="bg-gray-100 font-serif">
     <h1 className="text-4xl">Hi!</h1>
     <BooksForm />
     <BooksList />
--- src/components/Book.js
+++ src/components/Book.js
@@ -4,10 +4,13 @@ import PropTypes from 'prop-types';
 const Book = ({
   id, title, category, removeBook,
 }) => (
-  <div className="border bg-white hover:border-blue w-3/4 m-auto my-4 p-3 flex">
-    <div className="">
-      <span className="text-gray-500">{category}</span>
-      <h3 className="text-gray-900 font-extrabold">{title}</h3>
+  <div className="border rounded-md bg-white hover:border-blue w-3/4 m-auto my-4 p-5 grid grid-cols-4 gap-5">
+    <div className="col-span-2">
+      <span className="text-gray-500 font-sans text-sm font-bold">
+        {category}
+      </span>
+      <h3 className="text-gray-900 font-extrabold text-3xl">{title}</h3>
+      <span className="text-blue-300">Author Name</span>
       <div className="divide-x text-blue-400 text-sm">
         <button className="px-2" type="button">
           Edit
@@ -20,9 +23,17 @@ const Book = ({
         </button>
       </div>
     </div>
-    <div>Update Progress</div>
-    <div>
-      <button className="bg-blue-500 py-1 px-4 text-white" type="button">
+    <div className="flex flex-col justify-center">
+      <p className="text-3xl">64%</p>
+      <p className="text-gray-400 text-sm">Completed</p>
+    </div>
+    <div className="p-2">
+      <p>Current Chapter</p>
+      <p>Chapter 3</p>
+      <button
+        className="bg-blue-500 rounded-sm py-1 px-4 text-white"
+        type="button"
+      >
         Update Progress
       </button>
     </div>
