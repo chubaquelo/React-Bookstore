8e25dd177794961181c450d83d370ae8d6d6701d
Sergio Obolevich <sergioobolevich@gmail.com>
2021-04-08 15:47:01 -0300

    Add Header component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Header from './Header';
 
 const App = () => (
-  <div>
-    <h1 className="text-4xl">Hi!</h1>
-    <BooksForm />
-    <BooksList />
-  </div>
+  <>
+    <Header />
+    <div>
+      <h1 className="text-4xl">Hi!</h1>
+      <BooksForm />
+      <BooksList />
+    </div>
+  </>
 );
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <Header className="sticky w-screen h-10 bg-white text-blue-500 flex flex-row align-middle">
+    <h1>BookStore</h1>
+  </Header>
+);
+
+export default Header;
