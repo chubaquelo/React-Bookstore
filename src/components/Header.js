7cc406f0aa1794af6695d207034e0096252796ec
EstebanP <epinczinger@gmail.com>
2021-04-15 17:52:18 -0300

    clean header, add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = ({ title }) => {
-  const [displayAlert, setDisplayAlert] = useState(false);
+  const isLoggedIn = useSelector(state => state.session[1]);
 
-  const notWorkingAlert = () => {
-    setDisplayAlert(true);
-    setTimeout(() => setDisplayAlert(false), 1500);
-  };
-
-  const alertBox = (
-    <div className="absolute flex items-center right-0 h-full w-full bg-red-400 border rounded-tl-md border-l-blue-800">
-      <p className="mx-auto text-white text-4xl text-center">
-        Not working yet..
-      </p>
-    </div>
-  );
   return (
     <header className="w-full h-12percent px-6 py-25 bg-white flex justify-between sticky top-0 z-50 shadow-lg">
-      {displayAlert ? alertBox : null}
       <div className="w-8/12 flex flex-row">
         <div className="flex flex-col justify-center mr-10">
-          <h1 className="text-blue-400 text-lg sm:text-xl lg:text-3xl text-bold font-bold cursor-pointer">
-            {title}
-          </h1>
+          <Link to="/">
+            <h1 className="text-blue-400 text-lg sm:text-xl lg:text-3xl text-bold font-bold cursor-pointer">
+              {title}
+            </h1>
+          </Link>
         </div>
         <nav className="hidden md:flex flex-col justify-center mt-2 text-xl">
           <ul className="flex flex-row">
-            <Link to="/">
-              BOOKS
-            </Link>
-            <button
-              type="button"
-              onClick={notWorkingAlert}
-              className="px-5 cursor-pointer"
-            >
-              CATEGORIES
-            </button>
+            <Link to="/">BOOKS</Link>
           </ul>
         </nav>
       </div>
       <div className="w-4/12 flex flex-row justify-end my-auto">
-        <div className="h-12 w-12 rounded-full bg-gray-100 border border-gray-300 text-center flex flex-col justify-center cursor-pointer hover:bg-gray-300">
-          <Link to="/login">
+        {!isLoggedIn && (
+          <div className="h-12 w-12 rounded-full bg-gray-100 border border-gray-300 text-center flex flex-col justify-center cursor-pointer hover:bg-gray-300">
+            <Link to="/login">
+              <span role="img" aria-label="profile" className="text-2xl">
+                👱🏼
+              </span>
+            </Link>
+          </div>
+        )}
+
+        {isLoggedIn && (
+          <div className="ml-2 h-12 w-12 rounded-full bg-gray-100 border border-gray-300 text-center flex flex-col justify-center cursor-pointer hover:bg-gray-300">
             <span role="img" aria-label="profile" className="text-2xl">
-              👱🏼
+              ❌
             </span>
-          </Link>
-        </div>
+          </div>
+        )}
       </div>
     </header>
   );
